36e9c10cd6cd69de28c5c4a4d183b0d02e09c193
An Nguyen <nguyen.an.1196@gmail.com>
2024-04-24 23:25:42 -0700

    Embed google form

--- src/components/preorder/Preorder.js
+++ src/components/preorder/Preorder.js
@@ -1,7 +1,20 @@
+import React from 'react';
+
 function PreOrder() {
   return (
     <div>
-      <h1>This is the preorder page</h1>
+      <h2>April 27 - 28 Pickup</h2>
+      {/* Paste the Google Form iframe code here */}
+      <iframe 
+        src="https://forms.gle/uT8o3n2nHZXWVrDV7" 
+        width="100%" 
+        height="800" 
+        frameborder="0" 
+        marginheight="0" 
+        marginwidth="0"
+      >
+        Loading…
+      </iframe>
     </div>
   );
 }
