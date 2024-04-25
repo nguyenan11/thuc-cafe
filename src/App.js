782e4b8fda79d29d34fc92f5774369769162679f
An Nguyen <nguyen.an.1196@gmail.com>
2024-04-24 19:24:28 -0700

    Add logo to home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />}></Route>
+					{/* <Route path="/about" element={<About />}></Route> */}
+				</Routes>
+			</BrowserRouter>
     </div>
   );
 }
---
+++ src/components/home/Home.js
@@ -0,0 +1,19 @@
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import thucLogo from '../../media/images/thucLogo.png';
+
+function Home() {
+  return (
+  <Container>
+      <Row>
+        <Col xs={12} md={12}>
+          <Image src={thucLogo} />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Home;
